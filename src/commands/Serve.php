87a0f39b32b6929782fa38ecbea1448ebc77342f
James Ainooson <jainooson@gmail.com>
2016-06-27 12:22:15 +0000

    Fixed server command so options work as expected

--- src/commands/Serve.php
+++ src/commands/Serve.php
@@ -13,11 +13,13 @@ class Serve implements \clearice\Command
             'options' =>[
                 [
                     'short' => 'p', 'long' => 'port', 'has_value' => true, 
-                    'help' => 'Server port value (defaults to 8080)'
+                    'help' => 'Server port value (defaults to 8080)',
+                    'default' => '8080'
                 ],
                 [
                     'short' => 'h', 'long' => 'host', 'has_value' => true, 
-                    'help' => 'Host to be bound to (defaults to 127.0.0.1)'
+                    'help' => 'Host to be bound to (defaults to 127.0.0.1)',
+                    'default' => '127.0.0.1'
                 ]
             ]
         ];
@@ -28,7 +30,7 @@ class Serve implements \clearice\Command
         $spec = [STDOUT, STDIN, STDERR];
         $pipes = [];
         $process = proc_open(
-            PHP_BINARY . " -S 127.0.0.1:8080 " . __DIR__ . "/../../utils/router.php", 
+            PHP_BINARY . " -S {$options['host']}:{$options['port']} " . __DIR__ . "/../../utils/router.php", 
             $spec, $pipes
         );
         while(proc_get_status($process)['running']) {
