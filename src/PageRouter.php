a5986fa467b30d6dc9f88c8483e39179ab275678
James Ainooson <jainooson@gmail.com>
2017-05-09 15:58:14 -0500

    Added templates for plain text exceptions

--- src/PageRouter.php
+++ src/PageRouter.php
@@ -25,8 +25,19 @@ new class {
         ob_clean();
         $reflection = new ReflectionClass($exception);
         TemplateEngine::prependPath(__DIR__ . '/../templates/pages');
+        foreach (headers_list() as $header) {
+            preg_match("/(?<header>.*):(?<value>.*)/", $header, $matches);
+            if($matches['header'] == 'Content-Type') {
+                if(trim($matches['value']) != 'text/html') {
+                    $template = 'plain-exception';
+                } else {
+                    $template = 'exception';
+                }
+                break;
+            } 
+        }
         print TemplateEngine::render(
-            'exception', 
+            $template, 
             [
                 "type" => $reflection->getName(),
                 "message" => $exception->getMessage(),
---
+++ templates/pages/plain-exception.tpl.php
@@ -0,0 +1,11 @@
+<?=
+
+$stream = defined('STDERR') ? STDERR : fopen("php://output", w);
+fputs($stream, "Ntentan Error!\n");
+fputs($stream, strip_tags("$message\n\n"));
+foreach ($stack_trace as $trace_item) {
+    fputs($stream, "{$trace_item["file"]} ({$trace_item["line"]})\t\t\t\t"
+        . $trace_item["class"] . unescape($trace_item["type"])
+        . $trace_item["function"] . "\n"
+    );
+}
