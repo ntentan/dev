8c20aa7f2d73dd873b00ab59cfeb3bed2815b84c
James Ainooson <jainooson@gmail.com>
2017-05-13 20:35:38 -0500

    Cleaning out the messages in the plain exception

--- templates/pages/plain-exception.tpl.php
+++ templates/pages/plain-exception.tpl.php
@@ -1,8 +1,7 @@
 <?=
-
 $stream = defined('STDERR') ? STDERR : fopen("php://output", w);
 fputs($stream, "Ntentan Error!\n");
-fputs($stream, strip_tags("$message\n\n"));
+fputs($stream, strip_tags(unescape($message) . "\n\n"));
 foreach ($stack_trace as $trace_item) {
     fputs($stream, "{$trace_item["file"]} ({$trace_item["line"]})\t\t\t\t"
         . $trace_item["class"] . unescape($trace_item["type"])
