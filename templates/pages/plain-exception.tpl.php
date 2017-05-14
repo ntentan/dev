0f117013ac3c255b6e874914e0c676976f838304
James Ainooson <jainooson@gmail.com>
2017-05-13 22:43:16 -0500

    Improved the looks of text mode exceptions

--- templates/pages/plain-exception.tpl.php
+++ templates/pages/plain-exception.tpl.php
@@ -1,10 +1,19 @@
-<?=
+<?php
 $stream = defined('STDERR') ? STDERR : fopen("php://output", w);
 fputs($stream, "Ntentan Error!\n");
-fputs($stream, strip_tags(unescape($message) . "\n\n"));
+fputs($stream, strip_tags($message . "\n\n"));
+$max = 0;
+$formated_trace = [];
 foreach ($stack_trace as $trace_item) {
-    fputs($stream, "{$trace_item["file"]} ({$trace_item["line"]})\t\t\t\t"
-        . $trace_item["class"] . unescape($trace_item["type"])
-        . $trace_item["function"] . "\n"
-    );
+    $new_item = [
+        'file' => "{$trace_item["file"]}:{$trace_item["line"]}",
+        'call' => $trace_item["class"] . unescape($trace_item["type"]) . $trace_item["function"]
+    ];
+    if(strlen($new_item['file']) > $max) {
+        $max = strlen($new_item['file']);
+    }
+    $formated_trace[] = $new_item;
+}
+foreach ($formated_trace as $trace_item) {
+    fputs($stream, sprintf("%-{$max}s\t%s\n", $trace_item['file'], $trace_item['call']));
 }
