d8063d6f771305ef08bbbace18e4f3df2f304a9d
James Ainooson <jainooson@gmail.com>
2017-10-01 16:59:08 -0500

    Removing the init commant ..

--- src/commands/Build.php
+++ src/commands/Build.php
@@ -6,22 +6,22 @@ use ntentan\dev\assets\AssetPipeline;
 
 class Build
 {
-    
+
     public static function getCommandOptions()
     {
         return [
-            'command' => 'build', 
-            'help' => 'Build the assets required for the application', 
+            'command' => 'build',
+            'help' => 'Build the assets required for the application',
             'class' => '\ntentan\dev\commands\Build'
         ];
     }
-    
+
     public function run($options)
     {
-        if(file_exists('asset_pipeline.php')){
+        if (file_exists('asset_pipeline.php')) {
             AssetPipeline::setup('public');
             require 'asset_pipeline.php';
-        }        
+        }
     }
 
 }
