c6c3860d5a98cfbbfd6a96d224025e2e048ed6d4
Ekow Abaka Ainooson <jainooson@gmail.com>
2020-04-01 19:42:44 -0500

    Builder now reads the new default directory

--- src/commands/Build.php
+++ src/commands/Build.php
@@ -2,15 +2,18 @@
 
 namespace ntentan\dev\commands;
 
+use clearice\io\Io;
 use ntentan\dev\assets\AssetPipeline;
 
 class Build
 {
     private $assetPipeline;
+    private $io;
 
-    public function __construct(AssetPipeline $assetPipeline)
+    public function __construct(Io $io, AssetPipeline $assetPipeline)
     {
         $this->assetPipeline = $assetPipeline;
+        $this->io = $io;
     }
 
     public function run($options)
@@ -18,6 +21,8 @@ class Build
         if (file_exists($options['asset-pipeline'])) {
             $this->assetPipeline->setup($options);
             require $options['asset-pipeline'];
+        } else {
+            $this->io->error("Cannot find asset pipeline {$options['asset-pipeline']}\n");
         }
     }
 }
