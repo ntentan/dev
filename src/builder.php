5176e1a14678f454c1bc84cc7ddbef72090ee34b
James Ekow Abaka Ainooson <jainooson@gmail.com>
2024-08-22 19:55:02 -0500

    Formatting

--- src/builder.php
+++ src/builder.php
@@ -5,6 +5,7 @@ use ntentan\dev\assets\builders\CopyBuilder;
 use ntentan\dev\assets\builders\SassBuilder;
 use ntentan\utils\Filesystem;
 use ScssPhp\ScssPhp\Compiler;
+use ntentan\dev\assets\builders\JsBuilder;
 
 
 function runAssetBuilder(array $config) {
@@ -15,15 +16,16 @@ function runAssetBuilder(array $config) {
     error_log("Looking for assets to rebuild.");
     
     AssetPipeline::setup($config);
-
     AssetBuilder::register("sass", function() use ($cachePath) {
         $builder = new SassBuilder(new Compiler());
         Filesystem::directory($cachePath)->createIfNotExists();
         $builder->setCachePath($cachePath); 
         return $builder;
     });
-
+    AssetBuilder::register("js", fn() => new JsBuilder());
     AssetBuilder::register("copy", fn() => new CopyBuilder());
+    
     require $pipeline;
+    
     AssetPipeline::run();
 }
